d3fad339d2d5a90c1966ab497510e727522e68ab
yoongfook <yoongfook.work@gmail.com>
2012-05-04 10:31:35 +0800

    images

---
+++ paypal-express.php
@@ -0,0 +1,5 @@
+<form action='expresscheckout.php' METHOD='POST'>
+<input name="payment_amount" type="hidden" value="20" />
+<button name='submit' class="button red nice radius" alt='Check out with PayPal'>Checkout </button>
+
+</form>
---
+++ paypal-form.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<form action="paypal.php" method="post">
+Amount<input name="paymentAmount" type="text" value="10.00"/><br>
+paymentType<input name="paymentType" type="text" value="sale"/><br>
+cardType<input name="cardType" type="text" value="MasterCard"/><br>
+txtCardNumber<input name="txtCardNumber" type="text" value="5453552231481525"/><br>
+hidmonth<input name="hidmonth" type="text"value="04"/><br>
+hidyear<input name="hidyear" type="text"value="2017"/><br>
+hidSecurityCode<input name="hidSecurityCode" type="text"value="440"/><br>
+hidShippingFirstName<input name="hidShippingFirstName" type="text" value="John"/><br>
+hidShippingLastName<input name="hidShippingLastName" type="text" value="Kueh"/><br>
+hidShippingEmail<input name="hidShippingEmail" type="text" value="john@getgspot.com"/><br>
+hidShippingcountry<input name="hidShippingcountry" type="text" value="US"/><br>
+<button type="submit">Submit</button>
+</form>
+</body>
+</html>        
